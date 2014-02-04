59cfeb76553d7f73e748ea59b5281345e7380d6e
Brandon Hayduk <bm.hayduk@gmail.com>
2014-02-03 22:39:04 -0700

    HttpClient Update

--- httpclient.py
+++ httpclient.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Modifications by Brandon Hayduk per cmput 410 lectures, slides, and lab materials
 # Copyright 2013 Abram Hindle
 #
-# Modifications by Brandon Hayduk per cmput 410 lectures, slides, and lab materials
-# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,10 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Do not use urllib's HTTP GET and POST mechanisms.
-# Write your own HTTP GET and POST
-# The point is to understand what you have to send and get experience with it
 
 import sys
 import socket
@@ -38,42 +33,12 @@ class HTTPRequest(object):
 class HTTPClient(object):
     #def get_host_port(self,url):
 
-    def parseURL(self, url):
-        """
-        Method to take a url and use the urlparse method to
-        analyze and store the relevant aspects of the url. 
-       
-        """
-        print "URL: %s" % url
-        self.u = urlparse(url)
-
-        #Get Path 
-        if self.u.path == "":
-            self.path = "/"
-        else:
-            self.path = self.u.path
-
-        
-        #get Query (set parameters - encoded) 
-        if self.u.query:
-         
-            self.parameters = self.u.query.split("&")
-
-            q = ""
-            for param in self.parameters:
-                p = param.split("=")
-                key = urllib.quote_plus(p[0])
-                
-                if q != "":
-                    q = q + "&"
-                q = q + key
-                if len(p) > 1:
-                    value = urllib.quote_plus(p[1])
-                    q = q + "=" + value
-            self.parameters = q
-        
-
+    
     def connect(self):
+        """
+        Use sockets to create a connection based on the 
+        pared URL analyzed in the parseURL method
+        """
         # Create the socket 
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -82,23 +47,22 @@ class HTTPClient(object):
             sys.exit()
 
         #Get the hostname and port from the parsed url 
-        self.host = self.u.hostname
-        self.port = self.u.port
+        self.host = self.url.hostname
+        self.port = self.url.port
     
         if self.port == None:
             self.port = 80
 
+        #Handle remote IP
         try:
             self.remote_ip = socket.gethostbyname(self.host)
         except socket.gaierror:
             print('Hostname \'' + host+ '\' could not be resolved')
             sys.exit()
-        
-        print("host by name is:"+ self.host )
-       # print("port is: " + self.port)
 
         self.socket.connect((self.remote_ip, self.port))
-        
+       
+
     def get_code(self, data):
         #validate
         if not data:
@@ -106,9 +70,10 @@ class HTTPClient(object):
 
         #code first
         code = int(data.split(' ',2)[1])
-        print("Responded with Code: %d" % code)
+       # print("Responded with Code: %d" % code)
         return code
      
+
     def get_headers(self,data):
         #?
         return None
@@ -120,8 +85,12 @@ class HTTPClient(object):
 
         return data.split("\r\n\r\n",2)[1]
 
-    # read everything from the socket
+  
     def recvall(self, sock):
+        """
+        Read everything from the socket
+
+        """
         buffer = bytearray()
         done = False
         while not done:
@@ -132,70 +101,108 @@ class HTTPClient(object):
                 done = not part
         return str(buffer)
 
-    def create_header(self, method):
 
-        if self.parameters:
-            length = len(self.parameters)
+    def parseURL(self, url):
+        """
+        Method to take a url and use the urlparse method to
+        analyze and store the relevant aspects of the url. 
+        """
+
+        print "URL: %s" % url
+        self.url = urlparse(url)
+       
+        #Get Path 
+        if self.url.path == "":
+            self.path = self.url.path + '/'
         else:
-            length = 0
+            self.path = self.url.path
+       
+        #Get Query from URL:
+        #If someone sends a query encode the relevant parts with quote_plus from urllib
+        if self.url.query:
+            self.tokens = self.url.query.split("&")
+            #
+            query = ""
+            for param in self.tokens:
+                p = param.split("=")
+                key = urllib.quote_plus(p[0])
+                
+                if query != "":
+                    query = query + "&"
+                query = query + key
+                if len(p) > 1:
+                    value = urllib.quote_plus(p[1])
+                    query = query + "=" + value
+            self.tokens = query
 
+
+    def create_header(self, method):
+        """
+        Create the header based on request type
+        
+        """
         header = ("%s %s HTTP/1.1\r\n" % (method, self.path))
         header += ("Host: %s:%d \r\n" % (self.host, self.port))
         header += ("User-Agent: Ubuntu VM \r\n")
         
         if method == 'POST':
             header += ("Content-Type: application/x-www-form-urlencoded\r\n")
-            header += ("Content-Length: %d \r\n" % length)
+            header += ("Content-Length: %d \r\n" % len(self.tokens))
             
-        header += ("Connection: \"close\"\r\n\r\n")
+        header += ("Connection: close\r\n\r\n")
 
         return header
+
 
     def GET(self, url, args=None):
 
         if args:
-            self.parameters = urllib.urlencode(args)
+            self.tokens = urllib.urlencode(args)
         else:
-            self.parameters = "" 
+            self.tokens = "" 
         
-
         #Step one: verify URL:
         self.parseURL(url)
         self.connect()
         
         header = self.create_header("GET")
-        self.socket.sendall(header.encode("UTF8"))
-
-        if self.parameters:
-            self.socket.sendall("%s\r\n\r\n" % self.parameters)
         
+        print(header)
+        self.socket.sendall(header.encode("UTF8"))
+       
+        if self.tokens:
+            self.socket.sendall("%s\r\n\r\n" % self.tokens)
+       
         #get response
         resp = self.recvall(self.socket)
-        
+       
         #grab info from response
         code = self.get_code(resp)
         body = self.get_body(resp)
+
         self.socket.close()
-        
-        #connection = self.connect(url)
+       
         return HTTPRequest(code, body)
 
     def POST(self, url, args=None):
 
         if args:
-            self.parameters = urllib.urlencode(args)
+            self.tokens = urllib.urlencode(args)
         else:
-            self.parameters = ""
+            self.tokens = ""
         
         self.parseURL(url)
         self.connect()
         
         header = self.create_header("POST")
+
+        print(header)
         self.socket.sendall(header.encode("UTF8"))
 
-        if self.parameters:
-            self.socket.sendall("%s\r\n\r\n" % self.parameters)
+        if self.tokens:
+            self.socket.sendall("%s\r\n\r\n" % self.tokens)
 
+        #Get Response and analyze
         resp = self.recvall(self.socket)
         code = self.get_code(resp)
         body = self.get_body(resp)
